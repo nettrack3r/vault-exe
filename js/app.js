33d4ded870bf1bee4d40daa98a73da015c4444fb
nettrack3r <nettrack3r@gmail.com>
2026-05-08 03:40:30 -0400

    Access Granted / Denied Changes

--- js/app.js
+++ js/app.js
@@ -1,47 +1,86 @@
 const keys = document.querySelectorAll(".key");
+
 const clearBtn = document.querySelector(".clear");
+
 const enterBtn = document.querySelector(".enter");
 
 const display = document.getElementById("display");
+
 const message = document.getElementById("message");
+
+const terminal = document.querySelector(".terminal");
 
 let code = "";
 
+/* SECRET CODE */
+
 const correctCode = "7319";
 
+/* NUMBER BUTTONS */
+
 keys.forEach((key) => {
+
   key.addEventListener("click", () => {
 
     if (code.length < 4) {
+
       code += key.textContent;
 
       updateDisplay();
+
     }
 
   });
+
 });
 
+/* CLEAR BUTTON */
+
 clearBtn.addEventListener("click", () => {
+
   code = "";
+
   updateDisplay();
 
   message.textContent = "CODE CLEARED";
+
+  terminal.classList.remove("denied");
+  terminal.classList.remove("granted");
+
 });
 
+/* ENTER BUTTON */
+
 enterBtn.addEventListener("click", () => {
+
+  terminal.classList.remove("denied");
+  terminal.classList.remove("granted");
+
+  /* CORRECT CODE */
 
   if (code === correctCode) {
 
     message.textContent = "ACCESS GRANTED";
+
     message.classList.remove("error");
+
     message.classList.add("success");
 
-    document.body.style.background = "#001b0f";
+    terminal.classList.add("granted");
 
-  } else {
+  }
+
+  /* WRONG CODE */
+
+  else {
 
     message.textContent = "ACCESS DENIED";
+
+    message.classList.remove("success");
+
     message.classList.add("error");
+
+    terminal.classList.add("denied");
 
     shakeScreen();
 
@@ -53,19 +92,22 @@ enterBtn.addEventListener("click", () => {
 
 });
 
+/* UPDATE DISPLAY */
+
 function updateDisplay() {
 
-  display.textContent =
-    code.padEnd(4, "-");
+  display.textContent = code.padEnd(4, "-");
 
 }
 
-function shakeScreen() {
+/* SHAKE ANIMATION */
 
-  const terminal = document.querySelector(".terminal");
+function shakeScreen() {
 
   terminal.animate([
     { transform: "translateX(0px)" },
+    { transform: "translateX(-10px)" },
+    { transform: "translateX(10px)" },
     { transform: "translateX(-10px)" },
     { transform: "translateX(10px)" },
     { transform: "translateX(0px)" }
